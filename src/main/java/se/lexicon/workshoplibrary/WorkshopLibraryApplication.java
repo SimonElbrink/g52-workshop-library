738a1c4ec60742b7227c101b5cf8f10e2a3823b1
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-17 12:08:00 +0100

    Library workshop - Added CommandLineRunner

--- src/main/java/se/lexicon/workshoplibrary/WorkshopLibraryApplication.java
+++ src/main/java/se/lexicon/workshoplibrary/WorkshopLibraryApplication.java
@@ -1,7 +1,11 @@
 package se.lexicon.workshoplibrary;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import se.lexicon.workshoplibrary.repository.AppUserRepository;
+import se.lexicon.workshoplibrary.repository.DetailsRepository;
 
 @SpringBootApplication
 public class WorkshopLibraryApplication {
@@ -9,5 +13,14 @@ public class WorkshopLibraryApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkshopLibraryApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner runner(DetailsRepository detailsRepository, AppUserRepository userRepository) {
+        return args -> {
+            // Interact with App/Repos here.
+        };
+    }
+
 
 }
